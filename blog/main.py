dcdecc6ef479dfad9b6050a994d5faf79d51e9b3
prabhat <prabhat852981@gmail.com>
2022-11-05 23:13:13 +0530

    3rd commit

--- blog/main.py
+++ blog/main.py
@@ -1,11 +1,77 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, Response, status,HTTPException
 from pydantic import BaseModel
+from . import models, schemas
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
+
+
 app= FastAPI()
-class Blog(BaseModel):
-    title :str
-    body:str
-    
+
+from . import schemas, models
+
+# used to migrate models to database in table
+
+models.Base.metadata.create_all(engine)
+
+def get_db():
+    db= SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @app.post('/blog')
-def create(request:Blog):
-    return request
+def create(request:schemas.Blog, db:Session= Depends(get_db)):
+    new_blog= models.Blog(title=request.title, body= request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+@app.get('/blog')
+def all(db:Session= Depends(get_db)):
+    blogs=db.query(models.Blog).all()
+    return blogs
+
+
+
+@app.get('/blog/{id}', status_code=200)
+def show(id,response:Response ,db:Session= Depends(get_db)):
+    blog= db.query(models.Blog).filter(models.Blog.id==id).first()
+    if not blog:
+        response.status_code=status.HTTP_404_NOT_FOUND
+    return blog
+
+
+@app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED)
+def update(id,request: schemas.Blog, db:Session= Depends(get_db)):
+    
+    blog=db.query(models.Blog).filter(models.Blog.id==id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Blog with id {id} not found")
+    else:
+        blog.update(request)
+        db.commit()
+        return 'updated'
+    
+    
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id,db:Session= Depends(get_db)):
+    blog=db.query(models.Blog).filter(models.Blog.id==id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Blog with id {id} not found")
+    else:
+        blog.delete(synchronize_session=False)
+        db.commit()
+        return 'deleted'
+    
+    
+    
+
+
+
+
+
+
 
