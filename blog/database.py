a944e868c232746b38b90660e3608fa41f45ea66
prabhat <prabhat852981@gmail.com>
2022-11-17 17:53:48 +0530

    5th commit

--- blog/database.py
+++ blog/database.py
@@ -9,5 +9,5 @@ engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
+#new clone
 Base = declarative_base()
